2a4a8289723672e4626119e373fd7bc1f44488fc
Blake <telefragent@gmail.com>
2019-11-04 17:52:54 -0500

    Initial commit

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+	public Animator Animator;
+	public float WalkSpeed;
+
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+		{
+			transform.position += new Vector3(-WalkSpeed, 0f, 0f) * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += new Vector3(WalkSpeed, 0f, 0f) * Time.deltaTime;
+		}
+	}
+}
