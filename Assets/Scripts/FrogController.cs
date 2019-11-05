7d9677707429b9d2eaf1f62e379edbd8cbce9c8c
Blake <telefragent@gmail.com>
2019-11-04 23:01:33 -0500

    student

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class FrogController : MonoBehaviour
 {
 	public Animator Animator;
+	public AudioSource AudioSource;
 	public float WalkSpeed;
-	
-    void Update()
+
+	void Update()
     {
+		//Move left
         if(Input.GetKey(KeyCode.A))
 		{
 			transform.position += new Vector3(-WalkSpeed, 0f, 0f) * Time.deltaTime;
 
 		}
+		//Move right
 		else if (Input.GetKey(KeyCode.D))
 		{
 			transform.position += new Vector3(WalkSpeed, 0f, 0f) * Time.deltaTime;
 
 		}
+		//Idle
 		else
 		{
 			
